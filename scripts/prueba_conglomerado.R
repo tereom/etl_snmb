743fdbcf74793086ce014c47925231befac02940
tereom <teresa.ortiz.mancera@gmail.com>
2015-05-07 11:37:47 -0500

    Agregar comentarios

--- scripts/prueba_conglomerado.R
+++ scripts/prueba_conglomerado.R
@@ -1,42 +1,88 @@
+# El paquete RSQLite se utiliza para conectarse a las bases de datos (fases de
+# Extract y Load). El paquete de iterators se utilizará en la generación de id's
+library(RSQLite)
+library(iterators)
 library(dplyr)
-library(stringr)
 library(tidyr)
+library(stringr)
 
-base_input <- src_sqlite("../datos/base_input.db", create = T)
+######################################
+# Leyendo las bases de datos
+######################################
+# Leyendo la base de datos original
+base_input <- src_sqlite("../datos/base_input.db")
 
-### Leer tabla conglomerates
+# Creando la base de datos de salida
+base_output <- src_sqlite("../datos/base_output.db", create=T)
+######################################
 
+######################################
+# Instanciando los generadores de id's:
+######################################
+generador_conglomerado_muestra_id <- icount()
+# nextElem(conglomerado_muestra_id)
+generador_sitio_muestra_id <- icount()
+# nextElem(sitio_muestra_id)
+######################################
+
+######################################
+# Creando las tablas "Conglomerado_muestra" y "Sitio_muestra", a partir de la
+# tabla "conglomerates", como se especifica en el esquema: "mapeo_v2".
+######################################
+
+# Leyendo tabla conglomerates y transformándola a data frame
 conglomerates <- tbl(base_input, "conglomerates")
 conglomerates_df <- collect(conglomerates)
 
-conglomerado_muestra <- conglomerates_df %>%
+# Filtrando los conglomerados cuya clave tenga más de 5 dígitos (con errores)
+conglomerates_filtrado <- conglomerates_df %>%
   filter(nchar(name) <= 5)
+
+# Creando tabla "Conglomerado_muestra_id"
+#####################################
+
+# Generando "conglomerado_muestra_id":
+conglomerado_muestra_id <- c()
+for(i in 1:dim(conglomerates_filtrado)[1])
+{
+  conglomerado_muestra_id <- c(conglomerado_muestra_id, nextElem(generador_conglomerado_muestra_id))
+}
+
+# Agregando la nueva columna al data frame "conglomerates_filtrado"
+conglomerates_filtrado_id <- data.frame(conglomerates_filtrado, conglomerado_muestra_id)
+
+# Cambiando el nombre y tipo de las columnas, para hacerlas compatibles con el nuevo
+# cliente:
+
+#rename(conglomerates_filtrado_id, c("id"="conglomerate_id", "name"="nombre", "visit_date"="fecha_visita",))
 
 # Separar longitud, altitud, ...
 
-sitio_lat <- conglomerado_muestra %>%
-  select(name, central_lat, transect2_lat, transect3_lat, transect4_lat) %>%
-  gather(sitio, lat, -name) %>%
+sitio_lat <- conglomerates_filtrado_id %>%
+  select(id, conglomerado_muestra_id, central_lat, transect2_lat, transect3_lat, transect4_lat) %>%
+  gather(sitio, lat, -id, -conglomerado_muestra_id) %>%
+  mutate(sitio = str_extract(sitio, "[[:alnum:]]+"))
+# Gather crea por cada campo especificado, un registro, con dos campos (más los
+# campos no especificados como name): nombre del campo original y valor.
+
+sitio_lon <- conglomerates_filtrado_id %>%
+  select(id, conglomerado_muestra_id, central_lat, transect2_lat, transect3_lat, transect4_lat) %>%
+  gather(sitio, lat, -id, -conglomerado_muestra_id) %>%
   mutate(sitio = str_extract(sitio, "[[:alnum:]]+"))
 
-sitio_lon <- conglomerado_muestra %>%
-  select(name, central_lon, transect2_lon, transect3_lon, transect4_lon) %>%
-  gather(sitio, lon, -name) %>%
+sitio_altitude <- conglomerates_filtrado_id %>%
+  select(id, conglomerado_muestra_id, central_lat, transect2_lat, transect3_lat, transect4_lat) %>%
+  gather(sitio, lat, -id, -conglomerado_muestra_id) %>%
   mutate(sitio = str_extract(sitio, "[[:alnum:]]+"))
 
-sitio_altitude <- conglomerado_muestra %>%
-  select(name, central_altitude, transect2_altitude, transect3_altitude, transect4_altitude) %>%
-  gather(sitio, altitude, -name) %>%
+sitio_ellipsoid <- conglomerates_filtrado_id %>%
+  select(id, conglomerado_muestra_id, central_lat, transect2_lat, transect3_lat, transect4_lat) %>%
+  gather(sitio, lat, -id, -conglomerado_muestra_id) %>%
   mutate(sitio = str_extract(sitio, "[[:alnum:]]+"))
 
-sitio_ellipsoid <- conglomerado_muestra %>%
-  select(name, central_ellipsoid, transect2_ellipsoid, transect3_ellipsoid, transect4_ellipsoid) %>%
-  gather(sitio, ellipsoid, -name) %>%
-  mutate(sitio = str_extract(sitio, "[[:alnum:]]+"))
-
-sitio_gps_error <- conglomerado_muestra %>%
-  select(name, central_gps_error, transect2_gps_error, transect3_gps_error, transect4_gps_error) %>%
-  gather(sitio, gps_error, -name) %>%
+sitio_gps_error <- conglomerates_filtrado_id %>%
+  select(id, conglomerado_muestra_id, central_lat, transect2_lat, transect3_lat, transect4_lat) %>%
+  gather(sitio, lat, -id, -conglomerado_muestra_id) %>%
   mutate(sitio = str_extract(sitio, "[[:alnum:]]+"))
 
 sitios <- inner_join(sitio_lat, sitio_lon) %>%
@@ -44,8 +90,7 @@ sitios <- inner_join(sitio_lat, sitio_lon) %>%
   inner_join(sitio_ellipsoid) %>%
   inner_join(sitio_gps_error)
 
-sitios$date <- today()
+#sitios$date <- today()
 sitios
 
-copy_to(base_input, sitios, name = "sitios_fecha", temporary = FALSE)
-base_input
+copy_to(base_output, sitios, name = "sitios_id", temporary = FALSE)
